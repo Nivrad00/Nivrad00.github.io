98a7879e11696b570793305378ba0a91bd8ee28a
lib_ulcirc.com <lib_ulcirc.com@ad.unc.edu>
2020-02-21 13:01:13 -0500

    just little fixes

--- music.js
+++ music.js
@@ -5,23 +5,28 @@ musicData = [
 			{
 				"name": "1. Country Gentlemen & Noisekick - Fox on the Run (NIV'S MASHUP)",
 				"file": "Country Gentlemen & Noisekick - Fox on the Run (NIV'S MASHUP).mp3",
-				"date": "may 2018"
+				"date": "may 2018",
+				"duration": "2:11"
 			}, {
 				"name": "2. Jack and the Beanstalk",
 				"file": "Jack and the Beanstalk.mp3",
-				"date": "jun 2019"
+				"date": "jun 2019",
+				"duration": "2:11"
 			}, {
 				"name": "3. Skip James - Hard Time Killin' Floor Blues (NIV'S REMIX)",
 				"file": "Skip James - Hard Time Killin' Floor Blues (NIV'S REMIX).mp3",
-				"date": "jun 2019"
+				"date": "jun 2019",
+				"duration": "2:11"
 			}, {
 				"name": "4. The B-52's - Love Shack (NIV'S REMIX)",
 				"file": "The B-52's - Love Shack (NIV'S REMIX).mp3",
-				"date": "jun 2019"
+				"date": "jun 2019",
+				"duration": "2:11"
 			}, {
 				"name": "5. The Queenstons - Woodgrain (NIV'S REMIX)",
 				"file": "The Queenstons - Woodgrain (NIV'S REMIX).mp3",
-				"date": "jul 2019"
+				"date": "jul 2019",
+				"duration": "2:11"
 			}			
 		]
 	},
@@ -31,131 +36,163 @@ musicData = [
 			{
 				"name": "Feelin' Alright",
 				"file": "Feelin' Alright.mp3",
-				"date": "jan 2020"
+				"date": "jan 2020",
+				"duration": "2:11"
 			}, {
 				"name": "Happy Ending (WIP)",
 				"file": "Happy Ending (WIP).mp3",
-				"date": "jan 2020"
+				"date": "jan 2020",
+				"duration": "2:11"
 			}, {
 				"name": "Immobile",
 				"file": "Immobile.mp3",
-				"date": "jan 2020"
+				"date": "jan 2020",
+				"duration": "2:11"
 			}, {
 				"name": "Spacedeer OST",
 				"file": "spacedeer.mp3",
-				"date": "jan 2020"
+				"date": "jan 2020",
+				"duration": "2:11"
 			}, {
 				"name": "=3",
 				"file": "Meow =3.mp3",
-				"date": "dec 2019"
+				"date": "dec 2019",
+				"duration": "2:11"
 			}, {
 				"name": "Time Passes",
 				"file": "Time Passes.mp3",
-				"date": "dec 2019"
+				"date": "dec 2019",
+				"duration": "2:11"
 			}, {
 				"name": "The Bookless Room",
 				"file": "The Bookless Room.mp3",
-				"date": "nov 2019"
+				"date": "nov 2019",
+				"duration": "2:11"
 			}, {
 				"name": "Ghostly Grotto",
 				"file": "Ghostly Grotto.mp3",
-				"date": "aug 2019"
+				"date": "aug 2019",
+				"duration": "2:11"
 			}, {
 				"name": "Tom Johnson - Chord Catalog (Speedcore Remix)",
 				"file": "Tom Johnson - Chord Catalog (Speedcore Remix).mp3",
-				"date": "jul 2019"
+				"date": "jul 2019",
+				"duration": "2:11"
 			}, {
 				"name": "speedcore shepard tone",
 				"file": "speedcore shepard tone.mp3",
-				"date": "jul 2019"
+				"date": "jul 2019",
+				"duration": "2:11"
 			}, {
 				"name": "speedcore shepard tone 2",
 				"file": "speedcore shepard tone 2.mp3",
-				"date": "jul 2019"
+				"date": "jul 2019",
+				"duration": "2:11"
 			}, {
 				"name": "LET ME OUT OF HERE",
 				"file": "LET ME OUT OF HERE.wav",
-				"date": "mar 2019"
+				"date": "mar 2019",
+				"duration": "2:11"
 			}, {
 				"name": "Leave Me Alone",
 				"file": "leave me alone.wav",
-				"date": "mar 2019"
+				"date": "mar 2019",
+				"duration": "2:11"
 			}, {
 				"name": "dictionary (wip)",
 				"file": "dictionary (wip).m4a",
-				"date": "oct 2018"
+				"date": "oct 2018",
+				"duration": "2:11"
 			}, {
 				"name": "Funny Feeling",
 				"file": "funny feeling.m4a",
-				"date": "oct 2018"
+				"date": "oct 2018",
+				"duration": "2:11"
 			}, {
 				"name": "Aftermath",
 				"file": "Aftermath.m4a",
-				"date": "sep 2018"
+				"date": "sep 2018",
+				"duration": "2:11"
 			}, {
 				"name": "bottle up the air",
 				"file": "bottle up the air.m4a",
-				"date": "sep 2018"
+				"date": "sep 2018",
+				"duration": "2:11"
 			}, {
 				"name": "Love is Blind",
 				"file": "love is blind.m4a",
-				"date": "sep 2018"
+				"date": "sep 2018",
+				"duration": "2:11"
 			}, {
 				"name": "past and future (wip)",
 				"file": "past and future (wip).m4a",
-				"date": "sep 2018"
+				"date": "sep 2018",
+				"duration": "2:11"
 			}, {
 				"name": "The Falls at Tivoli",
 				"file": "The Falls at Tivoli.mp3",
-				"date": "mar 2018"
+				"date": "mar 2018",
+				"duration": "2:11"
 			}, {
 				"name": "shower thoughts (wip)",
 				"file": "shower thoughts 3-2-18.mp3",
-				"date": "mar 2018"
+				"date": "mar 2018",
+				"duration": "2:11"
 			}, {
 				"name": "Leonardo Dreams of His Flying Machine",
 				"file": "Leonardo Dreams of His Flying Machine.mp3",
-				"date": "dec 2017"
+				"date": "dec 2017",
+				"duration": "2:11"
 			}, {
 				"name": "bug in an octavo box",
 				"file": "11-15-17 bug in an octavo box.mp3",
-				"date": "nov 2017"
+				"date": "nov 2017",
+				"duration": "2:11"
 			}, {
 				"name": "LeonardosdGJREGJH",
 				"file": "LeonardosdGJREGJH.mp3",
-				"date": "nov 2017"
+				"date": "nov 2017",
+				"duration": "2:11"
 			}, {
 				"name": "Indoctrination",
 				"file": "Indoctrination.mp3",
-				"date": "oct 2017"
+				"date": "oct 2017",
+				"duration": "2:11"
 			}, {
 				"name": "hi, i'm steve rmx",
 				"file": "hi, i'm steve rmx.mp3",
-				"date": "aug 2017"
+				"date": "aug 2017",
+				"duration": "2:11"
 			}, {
 				"name": "The Songs of the Dead Are the Lamentations of the Living",
 				"file": "The Songs of the Dead Are the Lamentations of the Living.mp3",
-				"date": "jul 2017"
+				"date": "jul 2017",
+				"duration": "2:11"
 			}, {
 				"name": "My DM Won't Stop Making Stupid Jokes While My Character Dies Horribly",
 				"file": "My DM Won't Stop Making Stupid Jokes While My Character Dies Horribly.mp3",
-				"date": "jun 2017"
+				"date": "jun 2017",
+				"duration": "2:11"
 			}, {
 				"name": "View From a Car Window at Night",
 				"file": "View From a Car Window at Night.mp3",
-				"date": "jun 2017"
+				"date": "jun 2017",
+				"duration": "2:11"
 			}, {
 				"name": "The Precipice",
 				"file": "The Precipice.mp3",
-				"date": "apr 2017"
+				"date": "apr 2017",
+				"duration": "2:11"
 			}, {
 				"name": "Speedcore is the Eighth Deadly Sin",
 				"file": "Speedcore is the Eighth Deadly Sin.mp3",
-				"date": "apr 2017"
+				"date": "apr 2017",
+				"duration": "2:11"
 			}, {
 				"name": "Trumpets Are Evidently the Answer to Life's Greatest Questions (Instrumental)",
 				"file": "Trumpets Are Evidently the Answer to Life's Greatest Questions (Instrumental).mp3",
-				"date": "mar 2017"
+				"date": "mar 2017",
+				"duration": "2:11"
 			}
 		]
 	},
@@ -165,191 +202,238 @@ musicData = [
 			{
 				"name": "People of the Heavens",
 				"file": "People of the Heavens.mp3",
-				"date": "dec 2016"
+				"date": "dec 2016",
+				"duration": "2:11"
 			}, {
 				"name": "Smokesucker",
 				"file": "smokesucker.mp3",
-				"date": "nov 2016"
+				"date": "nov 2016",
+				"duration": "2:11"
 			}, {
 				"name": "9V rmx",
 				"file": "9V rmx.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "ambient project",
 				"file": "ambient project.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "amen doodle",
 				"file": "amen doodle.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Boss Rush Piano v2",
 				"file": "Boss Rush Piano v2.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "bright night",
 				"file": "bright night.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Cerulean Song",
 				"file": "Cerulean Song.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Choose Your Shot Type",
 				"file": "Choose Your Shot Type.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "The Death of Runnynosia",
 				"file": "The Death of Runnynosia.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Drifting",
 				"file": "Drifting.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Dusk Darter",
 				"file": "Dusk Darter.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Fantasia G",
 				"file": "Fantasia G.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Flying Turtles",
 				"file": "Flying Turtles.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Forward Fantasy",
 				"file": "Forward Fantasy.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Groundless",
 				"file": "Groundless.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Half Cataclysm",
 				"file": "Half Cataclysm.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "BANDETTO - higher higher higher (rmx)",
 				"file": "higher higher higher rmx.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "How to Love a Robot",
 				"file": "How to Love a Robot.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Ice Cream Truck Symphony",
 				"file": "Ice Cream Truck Symphony.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Jazz Advantage",
 				"file": "Jazz Advantage.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Koryo Garden",
 				"file": "Koryo Garden.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Lazy Town Hidden in the Marsh",
 				"file": "Lazy Town Hidden in the Marsh.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "mediocre expectations",
 				"file": "mediocre expectations.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Melodic Speedcore",
 				"file": "Melodic Speedcore.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Meg & Dia - Monster (rmx)",
 				"file": "Monster rmx.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Motrin Quartet",
 				"file": "Motrin Quartet.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Mystery in Sepia",
 				"file": "Mystery in Sepia.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "MYSTERY",
 				"file": "MYSTERY.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "New Town",
 				"file": "New Town.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Ode to Finchy",
 				"file": "Ode to Finchy.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Ostenato",
 				"file": "Ostenato.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Progression No.1 in G Major",
 				"file": "Progression No.1 in G Major.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Progression No.3 in G Minor",
 				"file": "Progression No.3 in G Minor.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Purple Waltz",
 				"file": "Purple Waltz.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Rejoice!",
 				"file": "Rejoice!.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Rhythmland",
 				"file": "Rhythmland.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "The Rose and the Nightingale",
 				"file": "The Rose and the Nightingale.mp3",
-				"date": "oct 2013"
+				"date": "oct 2013",
+				"duration": "2:11"
 			}, {
 				"name": "Smooth Sine",
 				"file": "Smooth Sine.mp3",
-				"date": "sep 2013"
+				"date": "sep 2013",
+				"duration": "2:11"
 			}, {
 				"name": "Soft Mice With No Ears Guard the Ice Artifact",
 				"file": "Soft Mice With No Ears Guard the Ice Artifact.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Space Elevator",
 				"file": "Space Elevator.mp3",
-				"date": "apr 2013"
+				"date": "apr 2013",
+				"duration": "2:11"
 			}, {
 				"name": "Stream No.9",
 				"file": "Stream No.9.mp3",
-				"date": "may 2013"
+				"date": "may 2013",
+				"duration": "2:11"
 			}, {
 				"name": "Stream No.12",
 				"file": "Stream No.12.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Sun-Kissed Fields",
 				"file": "Sun-Kissed Fields.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Triangle Waltz",
 				"file": "Triangle Waltz.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Trumpet Adventure",
 				"file": "Trumpet Adventure.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}, {
 				"name": "Untitled Dog Track",
 				"file": "Untitled dog track.mp3",
-				"date": ""
+				"date": "",
+				"duration": "2:11"
 			}
 		]
 	},
@@ -359,55 +443,68 @@ musicData = [
 			{
 				"name": "idk (wip)",
 				"file": "idk.wav",
-				"date": "sep 2019"
+				"date": "sep 2019",
+				"duration": "2:11"
 			}, {
 				"name": "Ghostly Grotto (SMW port)",
 				"file": "Ghostly Grotto (SMW port).mp3",
-				"date": "aug 2019"
+				"date": "aug 2019",
+				"duration": "2:11"
 			}, {
 				"name": "Cyberspace Serenade (SMW port)",
 				"file": "Cyberspace Serenade (SMW Port).mp3",
-				"date": "jul 2019"
+				"date": "jul 2019",
+				"duration": "2:11"
 			}, {
 				"name": "Sunset Escape (SMW original)",
 				"file": "Sunset Escape.mp3",
-				"date": "jul 2019"
+				"date": "jul 2019",
+				"duration": "2:11"
 			}, {
 				"name": "BeeSong",
 				"file": "BeeSong.wav",
-				"date": "jan 2019"
+				"date": "jan 2019",
+				"duration": "2:11"
 			}, {
 				"name": "chipper amen tune",
 				"file": "chipper amen tune.wav",
-				"date": "jan 2019"
+				"date": "jan 2019",
+				"duration": "2:11"
 			}, {
 				"name": "dramatic tune (wip)",
 				"file": "dramatic tune.wav",
-				"date": "jan 2019"
+				"date": "jan 2019",
+				"duration": "2:11"
 			}, {
 				"name": "chewy harmony (wip)",
 				"file": "chewy harmony.wav",
-				"date": "dec 2018"
+				"date": "dec 2018",
+				"duration": "2:11"
 			}, {
 				"name": "Boot Sector Breakdown",
 				"file": "speedcore thing (Boot Sector Breakdown).wav",
-				"date": "nov 2018"
+				"date": "nov 2018",
+				"duration": "2:11"
 			}, {
 				"name": "Cyberspace Serenade",
 				"file": "Cyberspace Serenade.wav",
-				"date": "nov 2018"
+				"date": "nov 2018",
+				"duration": "2:11"
 			}, {
 				"name": "Logic Gate Lullaby",
 				"file": "Logic Gate Lullaby.wav",
-				"date": "nov 2018"
+				"date": "nov 2018",
+				"duration": "2:11"
 			}, {
 				"name": "Malware Mayhem",
 				"file": "!! boss approaching !! (Malware Mayhem).wav",
-				"date": "nov 2018"
+				"date": "nov 2018",
+				"duration": "2:11"
 			}, {
 				"name": "Motherboard Melody",
 				"file": "my first beepbox (Motherboard Melody).wav",
-				"date": "nov 2018"
+				"date": "nov 2018",
+				"duration": "2:11"
 			}
 		]
 	}
