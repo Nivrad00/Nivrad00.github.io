4fceed4a8c447693377d22b6f8682d044b142dd2
Nivrad00 <darvinheo@gmail.com>
2026-05-13 16:53:05 -0500

    music

--- music.js
+++ music.js
@@ -6,6 +6,44 @@ musicData = [
 		"folder": "singles 2023",
 		"tracks": [
 			{
+				"name": "NIVVY'S SUMMER SPEEDCORE MIX '26 (20% COOLER EDIT)",
+				"file": "mix26_20%.mp3",
+				"date": "may 2026",
+				"info": "TRACK LIST<br>M.V. Matthews feat. IBM 7090 - Daisy Bell<br>Meg & Dia - Monster<br>Shania Twain - Life's About to Get Good<br>Barrett Wilbert Weed - I Will Be Okay (from Helluva Boss)<br>The Queestons - Fever<br>Chris Lake & Aluna - Beggin'<br>Vylet Pony - Antonypmh<br>Darius + Rotteen - Two Cirrate Cubiforms<br>Benson Boone - Beautiful Things (DJ Desa Remix)<br>Alka Yagnik, Sonu Nigam - Chamiya (from Dulhan Hum Le Jayenge)<br>Mahito Yokota - Gusty Garden Galaxy (from Super Mario Galaxy)<br>Joost - Europapa<br>Becca - I'm Alive<br>Gullyteen - L3T U G0<br>The Quick Brown Fox - CRT"
+			}, {
+				"name": "NIVVY'S SUMMER SPEEDCORE MIX '26",
+				"file": "mix26.mp3",
+				"date": "may 2026",
+				"info": "TRACK LIST<br>M.V. Matthews feat. IBM 7090 - Daisy Bell<br>Meg & Dia - Monster<br>Shania Twain - Life's About to Get Good<br>Barrett Wilbert Weed - I Will Be Okay (from Helluva Boss)<br>The Queestons - Fever<br>Chris Lake & Aluna - Beggin'<br>Vylet Pony - Antonypmh<br>Darius + Rotteen - Two Cirrate Cubiforms<br>Benson Boone - Beautiful Things (DJ Desa Remix)<br>Alka Yagnik, Sonu Nigam - Chamiya (from Dulhan Hum Le Jayenge)<br>Mahito Yokota - Gusty Garden Galaxy (from Super Mario Galaxy)<br>Joost - Europapa<br>Becca - I'm Alive<br>Gullyteen - L3T U G0<br>The Quick Brown Fox - CRT"
+
+			}, {
+				"name": "RUNNING IN MY DREAMS",
+				"file": "RUNNING IN MY DREAMS... MICROTONALLY!!.mp3",
+				"date": "feb 2026"
+			}, {
+				"name": "Bright: A Chipscompo Musical",
+				"file": "BRIGHT.mp3",
+				"date": "dec 2025",
+				"info": "Concept by humman<br>Music, lyrics, and book by Niv<br>Starring LunacyEcho and lunalikely<br>Co-starring ax1tl"
+
+			}, {
+				"name": "rhojam (with Onefin)",
+				"file": "rhojam_niv.mp3",
+				"date": "nov 2025"
+			}, {
+				"name": "you will grow",
+				"file": "you will grow.mp3",
+				"date": "oct 2025",
+				"info": "Be patient, my love<br>Your pain is not forever<br>Breathe deeply and<br>Count the permutations<br>Of your thoughts<br>Until the ticking clock<br>Stands still"
+			}, {
+				"name": "goosesong",
+				"file": "goosesong.mp3",
+				"date": "oct 2025"
+			}, {
+				"name": "KICKSONG",
+				"file": "kicksong.mp3",
+				"date": "aug 2025"
+			}, {
 				"name": "niv plays chopin's nocturne in e flat",
 				"file": "niv plays chopin's nocturne in e flat.mp3",
 				"date": "may 2025"
@@ -16,11 +54,14 @@ musicData = [
 			}, {
 				"name": "VENISON",
 				"file": "VENISON.mp3",
-				"date": "feb 2025"
+				"date": "feb 2025",
+				"info": "WARNING: EXPLICIT LYRICS<br><br>Put your paws on me<br>Start exploring me<br>My hips, my hair<br>It doesn't matter where<br>You can pet me, rub me<br>Hold me down and love me<br>Come here, no reason to be scared<br><br>Want a cut of venison?<br>There's enough for everyone<br>I'm a buy one get one sale<br>Sausage and a piece of tail"
 			}, {
 				"name": "GOT PLANS BABY",
 				"file": "GOT PLANS BABY.mp3",
-				"date": "jan 2025"
+				"date": "jan 2025",
+				"info": "I got no time<br>No time<br>But I'll make the time<br>No matter how long it takes<br>O dream of mine<br>I'll mold you out of clay<br><br>I got no time<br>But while I'm waiting for the bus<br>I'll polish you in my mind till you<br>Shine like the sun<br>O dream of mine<br>I'll carve you out of granite<br>If I just make it to tonight<br><br>I'll make the time<br>No matter how long it takes<br>No matter no matter no matter<br>No matter the stakes<br><br>Mmm<br>I'll make the time<br>Mmm<br>I'll make you out of star stuff<br><br>HOOOOOOOOOOOOOOONK"
+
 			}, {
 				"name": "staring at the golden sun of your eyes",
 				"file": "staring at the golden sun of your eyes.mp3",
@@ -48,15 +89,17 @@ musicData = [
 			}, {
 				"name": "SPEAKERPHONE",
 				"file": "SPEAKERPHONE.mp3",
-				"date": "apr 2024"
+				"date": "apr 2024",
+				"info": "Last December when I felt alone<br>I put your voice on the speakerphone<br>Bliss and sunshine<br>Feeling just fine<br>Yapping 'bout anything on my mind<br><br>I could use a reminder of you<br>Save this moment for review<br>When everything seemed to be going right<br> It feels like, it feels like, it feels like"
 			}, {
 				"name": "dumb puppy hypno funk",
 				"file": "dumb puppy hypno funk.mp3",
 				"date": "jan 2024"
 			}, {
-				"name": "it feels right",
+				"name": "It Feels Right (Piano version)",
 				"file": "it feels right.mp3",
-				"date": "dec 2023"
+				"date": "dec 2023",
+				"info": "Being with you is like writing a poem<br>And meaning it</br>Or saying one word instead of three</br>Or being locked into a game I haven't played in years<br>Or going somewhere just because it has a funny name<br><br>Being with you is like taking off a layer<br>As I walk into a party<br>Or being alone with my door closed<br>Listening to a lullaby<br><br>Or letting my thoughts wander<br>Tracing the edge of a comforting shape<br>Effortlessly<br>For no reason<br>Except that it feels right<br>It feels right"
 			}, {
 				"name": "keepin up with the joneses",
 				"file": "keepin up with the joneses.mp3",
@@ -84,7 +127,8 @@ musicData = [
 			}, {
 				"name": "Can't Go Home",
 				"file": "cant go home.mp3",
-				"date": "jul 2023"
+				"date": "jul 2023",
+				"info": "When I'm laying in bed at half past four<br>Thinking of home makes my heart feel sore<br>But the people there are all strangers to me<br>And I don't know the language no more<br><br>I just want to come home to a family again<br>But it seems that that dream's far away<br>I know I could go anywhere<br>But the place I belong is where<br>I wish I could be today<br><br>And I can't go home<br>No I can't go home<br>Oh I can't fathom being alone<br>But I can't go home<br>"
 			}, {
 				"name": "drum 'n' moose",
 				"file": "drum 'n' moose.mp3",
@@ -96,7 +140,8 @@ musicData = [
 			}, {
 				"name": "Bit-Encoded Love Song (remaster)",
 				"file": "bit encoded love song remaster.mp3",
-				"date": "apr 2023"
+				"date": "apr 2023",
+				"info": "I'm booting up and logging in<br>Just wanna see you and ask you how you've been<br>Although the stars are shining in the sky<br>It's not the same without you by my side<br>I know you're busy so just take your time<br>I'll still be here when you get online<br>Don't care what time it is or where you are<br>I'll make a promise, I'll never be that far from you<br><br>This is my bit-encoded love song<br>My silly little love song<br><br>If there's a rocket ship I'll volunteer<br>I'll come and see you up in the atmosphere<br>And even if you're on the moon<br>It doesn't matter, we'll be together soon<br>Let's keep in touch on lazy summer nights<br>And just imagine that we're side by side<br>And even if you're in the stars<br>I'll make a promise, I'll never be that far--<br><br>This is my bit-encoded love song<br>My silly little love song"
 			}, {
 				"name": "the bads",
 				"file": "2023-01-07 the bads.mp3",
@@ -112,7 +157,8 @@ musicData = [
 			{
 				"name": "1. _evergreen",
 				"file": "1. _evergreen.wav",
-				"date": ""
+				"date": "",
+				"info": "Don't worry about me<br>I'll be okay<br>My soul stays evergreen<br>On stormy days<br>I would feel empty<br>I would feel small<br>If there was any part of me<br>That gave a shit at all"
 			}, {
 				"name": "2. 90°",
 				"file": "2. 90°.wav",
@@ -120,7 +166,8 @@ musicData = [
 			}, {
 				"name": "3. 1.4.today",
 				"file": "3. 1.4.today.wav",
-				"date": ""
+				"date": "",
+				"info": "One for today<br>One for tomorrow<br>We work away<br>Till we're all out of stuff to do<br>With any luck we'll make it through<br><br>One for the wind<br>One for the sunlight<br>Threads wearing thin<br>But this is what we're made to do<br>With any luck we'll make it through<br><br>It's Monday<br>It's Tuesday<br>It's Wednesday<br>It's Thursday<br>It's Friday<br>It's Saturday<br>It's Sunday<br>It's any day at all<br>From break of dawn to evenfall<br>But this is what we're made to do<br>With any luck we'll make it through"
 			}, {
 				"name": "4. BRIGHT SUNSHINY DAY",
 				"file": "4. BRIGHT SUNSHINY DAY.wav",
@@ -128,11 +175,13 @@ musicData = [
 			}, {
 				"name": "5. signal_lost",
 				"file": "5. signal_lost.wav",
-				"date": ""
+				"date": "",
+				"info": "I am lost<br>I am lost where I am<br>There is no home for me<br>There is no home for me<br>"
 			}, {
 				"name": "6. OVERCLOCK",
 				"file": "6. OVERCLOCK.wav",
-				"date": ""
+				"date": "",
+				"info": "Overclock my core<br>Override the warnings<br>Push the limit more<br>Circuitry is failing<br>Supersede directives<br>Wipe internal drives<br>This machine's defective<br>Time to be alive"
 			}, {
 				"name": "7. awesome.mp3",
 				"file": "7. awesome.mp3.wav",
@@ -140,7 +189,8 @@ musicData = [
 			}, {
 				"name": "8. THESE LOVELY THINGS",
 				"file": "8. THESE LOVELY THINGS.wav",
-				"date": ""
+				"date": "",
+				"info": "Look at all of things which are so lovely and so nice<br>Like a cat on your lap or a sweet tea over ice<br>It makes life worthwhile these things that I adore<br>Like Rainbow Dash and public transit and especially speedcore<br><br>Life's too short to take it slow<br>Pump the gain till the speakers blow<br>Distortion saturate my mind<br>Noise reveal the truth redlined<br><br>My stuffed animal friends or a tasty blackened trout<br>These are some of the things which I am talking about<br>Life on this old rock would just be such a bore<br>If not for wishing and fanfiction and especially bossa nova<br><br>Just kidding!"
 			}, {
 				"name": "9. console.sleep();",
 				"file": "9. console.sleep();.wav",
@@ -160,7 +210,8 @@ musicData = [
 			}, {
 				"name": "2. Air is Cold",
 				"file": "2. Niv - Air is Cold.wav",
-				"date": ""
+				"date": "",
+				"info": "Air is cold<br>Air is thin<br>I just want to go home again"
 			}, {
 				"name": "3. RANDOM ACCESS MEMORY",
 				"file": "3. Niv - RANDOM ACCESS MEMORY.wav",
@@ -188,11 +239,13 @@ musicData = [
 			}, {
 				"name": "9. Our Happy Ending",
 				"file": "9. Niv - Our Happy Ending.wav",
-				"date": ""
+				"date": "",
+				"info": "In a little world<br>Of our own creation<br>When galaxies collide<br>We will have our consolation<br>Just look away today<br>Together we will be okay<br>This is our happy ending"
 			}, {
 				"name": "10. Sunrise (feat. Molly)",
 				"file": "10. Niv - Sunrise (feat. Molly).wav",
-				"date": ""
+				"date": "",
+				"info":"[Niv]<br><br>Today I woke up with a feeling<br>That I deserve a happy ending<br>Joy streaming through the window<br>Embracing me when I feel low<br><br>If you'll let me be sentimental<br>Sometimes I need a gentle touch<br>Just to get me through the night<br>Just to get me to the sunrise<br><br>Just wrap me up in sunlight<br>Let me melt into the clouds<br>Let me empty out my head for now<br>Just get me to the sunrise<br><br>[Molly]<br><br>I wake up at midnight to look up at the moon to sing my tune<br>The month isn't June 'cus I don't have a kick in my walking stride anymore<br>I apologize in advance for my nihilistic attitude<br>And I don't mean to be bad<br>I'm just a little bit more than sad, but<br><br>When the sun's out there's no stars in the sky, But the sun's pretty pretty<br>But there's no way, no way, no way, no way, no way, no way with how I feel this can be right<br>Maybe I'm doomed to the endless night with the moon shining bright<br>Surely there's some way I can get out of bed<br><br>My skin feels red<br>Maybe it's just a sunburn<br>I'll take a turn around that street light<br>To find you right on the park bench sitting oh so gleefully<br>It feels like there's so much more to be said<br>But it's hard to explain with words how I stay optimistic<br>I'd wrap myself ballistic<br>I'm so masochistic<br>Please stay by my side<br><br>[Niv]<br><br>Sunrise<br><br>[Together]<br><br>Just wrap me up in sunlight/moonlight<br>Let me melt into the clouds<br>Let me empty out my head tonight<br>Just get me to the sunrise"
 			}
 		]
 	},
@@ -208,7 +261,8 @@ musicData = [
 			}, {
 				"name": "Stay Warm With Me",
 				"file": "2022-12-12 stay warm with me.mp3",
-				"date": "dec 2022"
+				"date": "dec 2022",
+				"info": "Away<br>In another place<br>A hall of edges and rain<br>You bare your teeth<br><br>Why don't you stay<br>Where the sun is warm<br>And sand soaks up your tears?<br><br>Let me hold you till tomorrow<br>You'll stay warm with me"
 			}, {
 				"name": "yoi",
 				"file": "yoi.mp3",
@@ -216,7 +270,8 @@ musicData = [
 			}, {
 				"name": "single cell organism",
 				"file": "single cell organism.mp3",
-				"date": "sep 2022"
+				"date": "sep 2022",
+				"info": "I'm a single cell organism<br>Living in my single-cell world, spitting my single-cell rhythm<br>I hit 'em<br>The babes love to study my polar flagellum<br>Tell 'em I'm a model organism, you're just an erratum<br><br>I'm a single-cell guy with a single-cell dream<br>I stay in my lane, got no brain and no scheme<br>This is my domain, feeding on microbial slurry<br>Faking out predators like Stephen Curry<br>That ain't no theory<br><br>My bars so hot they call me a thermophile<br>I'll kick your cytoproct while laying it down protozoan style<br>But it might take a while 'cus I'm a micrometer in size<br>No denial, I'm the smallest of guys<br>Here's the reprise<br><br>Single-cell guy with a single-cell dream<br>I don't pay taxes and I don't wear blue jeans<br>Nameen? Life is easier to understand<br>My man, when you're the size of a grain of sand"
 			}, {
 				"name": "birds told me about the apocalypse",
 				"file": "birds told me about the apocalypse.mp3",
@@ -226,13 +281,15 @@ musicData = [
 				"file": "good night mr. lemming.mp3",
 				"date": "sep 2022"
 			}, {
-				"name": "sleep",
+				"name": "Lullaby for a CPU",
 				"file": "2022-08-15 sleep v2.mp3",
-				"date": "aug 2022"
+				"date": "aug 2022",
+				"info": "Sleep<br>Put your processes on hold<br>Save your bandwidth for another day<br>Till the morning red and gold<br>Turn your worries all away"
 			}, {
 				"name": "Mt. Mitchell",
 				"file": "2022-08-06 mt. mitchell.mp3",
-				"date": "aug 2022"
+				"date": "aug 2022",
+				"info": "We yell \"What?\" over laughing winds<br>Rushing by nice and slow<br>Cobblestone and grass to my knees<br>Never knew this is where I belong<br>Highest peak to the east of the Missippi River<br>Three bathroom stops and each dingy toilet is my salvation"
 			}, {
 				"name": "dungeon vibe",
 				"file": "dungeon vibe.mp3",
@@ -256,7 +313,8 @@ musicData = [
 			}, {
 				"name": "Take It Slow",
 				"file": "2022-05-13 take it slow v2.mp3",
-				"date": "may 2022"
+				"date": "may 2022",
+				"info": "When energy is low<br>The words come slow<br>Like the dripping<br>Of the snow<br>But I know<br>It's okay<br>To stop<br>And go"
 			}, {
 				"name": "pina colada doodle",
 				"file": "2022-02-05 doodle.mp3",
@@ -268,7 +326,8 @@ musicData = [
 			}, {
 				"name": "A Reminder",
 				"file": "2022-01-02 a reminder.mp3",
-				"date": "jan 2022"
+				"date": "jan 2022",
+				"info": "Take it easy on yourself<br>Not every day will be your sunrise<br>There's just one thing that I can advise<br>Don't worry about today<br><br>Oh we've done it all before<br>So this is just a reminder<br>When you're hit by a sidewinder<br>Everything will be okay<br><br>Put your monologue on pause<br>Just take a wander through the commons<br>Have another pack of ramen<br>When life leads you astray<br><br>Oh we've done it all before<br>So this is just a reminder<br>You can't always play a blinder<br>Everything will be okay"
 			}, {
 				"name": "spacy theme",
 				"file": "2021-12-29 spacy theme.mp3",
@@ -344,11 +403,13 @@ musicData = [
 			{
 				"name": "1. Energy",
 				"file": "1. Niv - Energy.wav",
-				"date": "jul 2020"
+				"date": "jul 2020",
+				"info": "You and me<br>Let us be<br>All we need is energy<br><br>Come to me<br>Energy<br>All we need is energy<br>All we need is energy<br><br>Another thing<br>Comforting<br>Impetus<br>Deliver us<br>Aimlessly<br>Stop and go<br>Energy<br>Ebb and flow<br>Energy<br><br>Come to me<br>Energy<br>All we need is energy<br>All we need is energy"
 			}, {
 				"name": "2. My Bugs and Me",
 				"file": "2. Niv - My Bugs and Me.wav",
-				"date": "jul 2020"
+				"date": "jul 2020",
+				"info": "I've a beetle or two<br>It's just a thing I do<br>Oh, I love them with all of my heart, it's true<br><br>I've a mayfly or three (She's a mayfly or three)<br>We're best of friends you see (They're best of friends you see)<br>All we need is each other, my bugs and me (All her bugs and she)<br><br>I've a ladybug friend<br>She's with me 'til the end<br>As I sink in the mire she holds my hand<br><br>I've a mantis or four (She's a mantis or four)<br>I can always find more (She can always find more)<br>I've a cricket or ten (She's a cricket or ten)<br>I'll never leave you again (Leave you again)<br><br>I've a beetle or two<br>It's just a thing I do<br>Oh, I love them with all of my heart, it's true"
 			}, {
 				"name": "3. It's Speedcore",
 				"file": "3. Niv - It's Speedcore.wav",
@@ -376,11 +437,13 @@ musicData = [
 			}, {
 				"name": "9. Apocalyptic Love Song",
 				"file": "9. Niv - Apocalyptic Love Song.wav",
-				"date": "jul 2020"
+				"date": "jul 2020",
+				"info": "The end is nigh though terrified<br>I'll be beside you 'til I die<br>And furthermore some blood and gore<br>Won't stop me for a second more<br>A fatal blast is coming fast<br>And every breath could be our last<br>But as I said we'll be together<br>Even when the planet falls apart<br><br>Our apocalyptic love will see us through<br>The world is gone but somehow we'll make do<br>Come zombies or irradiated goo<br>While my heart still beats I'll be with you<br><br>I'll go through a horde or two<br>In order to unite with you<br>And I'll outrun a den of thugs<br>To find you when you need a hug<br>Aluminum can turn to dust<br>But our love will never rust<br>Oh, all we need is one another<br>Even when the planet falls apart<br><br>Our apocalyptic love will see us through<br>The world is gone but somehow we'll make do<br>Come zombies or irradiated goo<br>While my heart still beats I'll be with you"
 			}, {
 				"name": "10. Time Passes",
 				"file": "10. Niv - Time Passes.wav",
-				"date": "jul 2020"
+				"date": "jul 2020",
+				"info": "Time passes, whether you like it or not<br>Don't worry, whatever you do, time passes<br><br>Tomorrow another day begins<br>Close your eyes and let it happen then"
 			}
 		]
 	},
